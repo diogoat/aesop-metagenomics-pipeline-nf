52dc3aaaad1c51afa23e9d9f7dda58b0ff865401
Diogo Tschoeke <129115019+diogoat@users.noreply.github.com>
2026-05-14 12:11:30 -0300

    Delete src directory

--- src/databases_setup/script_metadata_accession_map.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-input_folder=$1
-
-for file in $input_folder/*.tsv; do
-  awk -F "\t" '{print $1}' "$file" | awk 'NR==FNR {acc[$1]; next} $1 in acc' - accession_taxid.tmp > "${file%.tsv}_map.txt"
-done
